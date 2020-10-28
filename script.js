0bdaa712b0d079a052afb33efb3807a9067bc20a
Aliaksandr Akhremchyk <Alex_1984_05@mail.ru>
2020-10-28 13:54:05 +0300

    refactor: fixed bugs

--- script.js
+++ script.js
@@ -1,15 +1,71 @@
+// Menu active
 const MENU = document.getElementById('navmain');
 MENU.addEventListener('click', (event) =>{
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
 
     event.target.classList.add('active');
-    document.getElementById('overlay-tint').classList.add('hiden');
+    document.getElementById('overlay-tint').classList.add('hidden');
     document.getElementById('burger-menu').classList.remove('move');
     document.getElementById('menu-icon').classList.remove('doun');
-    document.getElementById('buttun').classList.remove('rotate');
-    document.getElementById('h1').classList.remove('hiden');
+    document.getElementById('button').classList.remove('rotate');
+    document.getElementById('h1').classList.remove('hidden');
 });
 
+// Menu burger
+const MENUC = document.getElementById('button');
+MENUC.addEventListener('click', () =>{
+    if(MENUC.getAttribute('class') == 'rotate') {
+        document.getElementById('overlay-tint').classList.add('hidden');
+        document.getElementById('burger-menu').classList.remove('move');
+        document.getElementById('menu-icon').classList.remove('doun');
+        document.getElementById('button').classList.remove('rotate');
+        document.getElementById('h1').classList.remove('hidden');
+    } else {
+    document.getElementById('h1').classList.add('hidden');
+    document.getElementById('overlay-tint').classList.remove('hidden');
+    document.getElementById('burger-menu').classList.add('move');
+    document.getElementById('menu-icon').classList.add('doun');
+    MENUC.classList.add('rotate');
+    }
+});
+
+window.addEventListener(`resize`, event => {
+    if(window.innerWidth > 768 && (MENUC.getAttribute('class') == 'rotate')) {
+        document.getElementById('overlay-tint').classList.add('hidden');
+        document.getElementById('burger-menu').classList.remove('move');
+        document.getElementById('menu-icon').classList.remove('doun');
+        document.getElementById('button').classList.remove('rotate');
+        document.getElementById('h1').classList.remove('hidden');
+    }
+
+
+});
+
+let heightHeader = 95;
+document.addEventListener('scroll', onScroll);
+    function onScroll() {
+    const POS = window.scrollY;
+    const DIVS = document.querySelectorAll('main>div');
+    if(window.innerWidth > 768) {
+        heightHeader = 95;
+    } else {
+        heightHeader = 76;
+    }
+
+    DIVS.forEach((el) => {
+        if(el.offsetTop - heightHeader <= POS && (el.offsetTop - heightHeader + el.offsetHeight) > POS){
+            MENU.querySelectorAll('a').forEach((a) => {
+                a.classList.remove('active');
+                if (el.getAttribute('class').slice(0, 4) === a.getAttribute('class').slice(0, 4)) {
+                    a.classList.add('active');
+                }
+            })
+        }
+    });
+}
+
+
+// Menu tab
 const TAB = document.getElementById('tab');
 const MIXED = document.getElementById('grid2');
 let arr1 = MIXED.querySelectorAll('img');
@@ -22,19 +78,13 @@ TAB.querySelectorAll('li').forEach(el => {
 el.addEventListener('click', (event) =>{
     TAB.querySelectorAll('li').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
-   
-    // MIXED.querySelectorAll('img').forEach(el => el.classList.remove('border-img'));
     MIXED.querySelectorAll('img').forEach(el => el.remove());
-    // function mix(arr1){
-        for (var i = arr1.length - 1; i > 0; i--) {
+    for (var i = arr1.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
             var temp = arr1[i];
             arr1[i] = arr1[j];
             arr1[j] = temp;
         }
-    // }
-    // mix(arr1); 
-    // let arr2 = [];
     while (i < arr1.length) {
         var j = Math.floor(Math.random() * (arr1.length));
         if (arr2[j] == a){
@@ -47,6 +97,8 @@ el.addEventListener('click', (event) =>{
 });
 });
 
+
+// Border portfolio
 MIXED.querySelectorAll('img').forEach(el => {
 el.addEventListener('click', (event) => {
     MIXED.querySelectorAll('img').forEach(el => el.classList.remove('border-img'));
@@ -54,52 +106,8 @@ el.addEventListener('click', (event) => {
     });
 });
 
-const MENUC = document.getElementById('buttun');
-MENUC.addEventListener('click', () =>{
-    if(MENUC.getAttribute('class') == 'rotate') {
-        document.getElementById('overlay-tint').classList.add('hiden');
-        document.getElementById('burger-menu').classList.remove('move');
-        document.getElementById('menu-icon').classList.remove('doun');
-        document.getElementById('buttun').classList.remove('rotate');
-        document.getElementById('h1').classList.remove('hiden');
-    } else {
-    document.getElementById('h1').classList.add('hiden');
-    document.getElementById('overlay-tint').classList.remove('hiden');
-    document.getElementById('burger-menu').classList.add('move');
-    document.getElementById('menu-icon').classList.add('doun');
-    MENUC.classList.add('rotate');
-    }
-});
 
-window.addEventListener(`resize`, event => {
-    if(window.innerWidth > 768 && (MENUC.getAttribute('class') == 'rotate')) {
-        document.getElementById('overlay-tint').classList.add('hiden');
-        document.getElementById('burger-menu').classList.remove('move');
-        document.getElementById('menu-icon').classList.remove('doun');
-        document.getElementById('buttun').classList.remove('rotate');
-        document.getElementById('h1').classList.remove('hiden');
-    }
-
-});
-
-
-document.addEventListener('scroll', onScrol);
-function onScrol() {
-    const POS = window.scrollY;
-    const DIVS = document.querySelectorAll('main>div');
-
-    DIVS.forEach((el) => {
-        if(el.offsetTop - 95 <= POS && (el.offsetTop - 95 + el.offsetHeight) > POS){
-            MENU.querySelectorAll('a').forEach((a) => {
-                a.classList.remove('active');
-                if (el.getAttribute('class').slice(0, 4) === a.getAttribute('class').slice(0, 4)) {
-                    a.classList.add('active');
-                }
-            })
-        }
-    });
-}
-
+// SLIDER
 const LEFT = document.getElementById('arrow-left');
 const RIGHT = document.getElementById('arrow-right');
 const SLIDE1 = document.getElementById('slide-1');
@@ -154,12 +162,13 @@ LEFT.addEventListener('click', () =>{
     autoClick = setInterval(clickLeft, 7000);
 });
 
+// Display phone active
 const PHONEV = document.getElementById('phone-vertical');
 const DISPLAY = document.getElementById('vertical-display');
 PHONEV.addEventListener('click', () =>{
-    if(DISPLAY.getAttribute('class').indexOf('hiden', 0) > 0) {
-        DISPLAY.classList.remove('hiden');
+    if(DISPLAY.getAttribute('class').indexOf('hidden', 0) > 0) {
+        DISPLAY.classList.remove('hidden');
     } else {
-        DISPLAY.classList.add('hiden');
+        DISPLAY.classList.add('hidden');
     }
 });
